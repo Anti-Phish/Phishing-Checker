1a915a0c4fc9d0f6c6e52a1d004df16139037134
Wikum Chamith <wikum@linuxdeveloper.space>
2021-08-09 21:56:26 +0530

    API documentaiton

--- app.py
+++ app.py
@@ -14,7 +14,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to Anti-Phishing'
+    return 'Welcome to Anti-Phishing<br>' \
+           'Send a POST request to /check endpoint to get info about the url. <br>' \
+           '<h3> POST Body </h3><br>' \
+           '{<br>' \
+           '    "url": "https://example.com/path</br>'\
+           '}<br>' \
+           '<h3> Response code meaning </h3> <br>'\
+           '<ol>' \
+           '<li>Totally unsafe. Already confirmed phishing link.</li>' \
+           '<li>Unsafe. ML model identity as a phishing link. Discretion is advised.</li>' \
+           '<li>Generally safe, ML Model identify as safe but discretion is advised.</li>' \
+           '<li>Safe. Already a well established site.</li>' \
+           '</ol>'
 
 
 @app.route('/check', methods=['POST'])
@@ -23,15 +35,21 @@ def check_url():
     domain = urlparse(url).netloc
     if top_sites.find_one({"url": domain}):
         print("Top mil site")
-        return {"status": "good"}
+        return {"status": "4"}  # Safe
 
     db_check = db.find_one({"url": url})
     if db_check is not None:
         print("Found on Database")
-        return str(db_check["Ok"])
+        if db_check["Ok"] == "bad":
+            return {"status": "1"}  # Already confirmed phishing site
+        else:
+            return {"status": "4"}  # Safe
 
     print("Found from model")
-    return str(model.predict(vectorizer.transform([url])))
+    if model.predict(vectorizer.transfor[url]) == ['bad']:
+        return {"status": "2"}  # Model identify as phishing link, Hard to confirm
+    else:
+        return {"status": "3"}  # Model identify as safe
 
 
 if __name__ == '__main__':
