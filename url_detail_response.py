7d662a0345551c8591e0ba3c1d7b8548c92b79ad
Wikum Chamith <wikum@linuxdeveloper.space>
2021-08-10 20:40:01 +0530

    Added request headers

--- url_detail_response.py
+++ url_detail_response.py
@@ -1,3 +1,6 @@
+import flask
+
+
 class UrlResponse:
     code_meaning = {1: ["Dangerous", "Detected from database"],
                     2: ["Unsafe", "Detected from model"],
@@ -5,6 +8,7 @@ class UrlResponse:
                     4: ["Safe", "Detected from database"]}
 
     def __init__(self, code):
-        self.response = {"threat_level": str(code),
-                         "description": self.code_meaning[code][0],
-                         "detected_from": self.code_meaning[code][1]}
+        self.response = flask.jsonify({"threat_level": str(code),
+                                       "description": self.code_meaning[code][0],
+                                       "detected_from": self.code_meaning[code][1]})
+        self.response.headers.add('Access-Control-Allow-Origin', '*')
