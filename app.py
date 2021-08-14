bea413a607506b80a4f0cf9e0776d0d67c2c55ec
Wikum Chamith <wikum@linuxdeveloper.space>
2021-08-14 10:32:36 +0530

    refactoring

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import joblib
 import pymongo as pymongo
 from flask import Flask, request
-from urllib.parse import urlparse
 from flask_cors import CORS
 from tldextract import extract
 
@@ -23,9 +22,9 @@ def welcome():
            'Send a POST request to /check endpoint to get info about the url. <br>' \
            '<h3> POST Body </h3><br>' \
            '{<br>' \
-           '    "url": "https://example.com/path</br>'\
+           '    "url": "https://example.com/path</br>' \
            '}<br>' \
-           '<h3> Response code meaning </h3> <br>'\
+           '<h3> Response code meaning </h3> <br>' \
            '<ol>' \
            '<li>Totally unsafe. Already confirmed phishing link.</li>' \
            '<li>Unsafe. ML model identity as a phishing link. Discretion is advised.</li>' \
@@ -37,32 +36,26 @@ def welcome():
 @app.route('/check', methods=['POST'])
 def check_url():
     url = request.json["url"]
-    # domain = urlparse(url).netloc
-    tsd, td, tsu = extract(url)  # prints abc, hostname, com
+    tsd, td, tsu = extract(url)
     domain = td + '.' + tsu
     print(domain)
     if top_sites.find_one({"url": domain}):
         print("Top mil site")
-        return UrlResponse(4).response
-        # return {"status": "4"}  # Safe
+        return UrlResponse(4).response  # Safe
 
     db_check = db.find_one({"url": url})
     if db_check is not None:
         print("Found on Database")
         if db_check["Ok"] == "bad":
-            return UrlResponse(1).response
-            # return {"status": "1"}  # Already confirmed phishing site
+            return UrlResponse(1).response  # Already confirmed phishing site
         else:
-            return UrlResponse(4).response
-            # return {"status": "4"}  # Safe
+            return UrlResponse(4).response  # Safe
 
     print("Found from model")
     if model.predict(vectorizer.transform([url])) == ['bad']:
-        return UrlResponse(2).response
-        # return {"status": "2"}  # Model identify as phishing link, Hard to confirm
+        return UrlResponse(2).response  # Model identify as phishing link, Hard to confirm
     else:
-        return UrlResponse(3).response
-        # return {"status": "3"}  # Model identify as safe
+        return UrlResponse(3).response  # Model identify as safe
 
 
 if __name__ == '__main__':
